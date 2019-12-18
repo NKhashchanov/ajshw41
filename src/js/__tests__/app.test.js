c2c335391756dd5ed2e3d5c0ea9901e70ba8604f
Николай <37711307+NKhashchanov@users.noreply.github.com>
2019-12-18 21:13:21 +0300

    up

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -5,6 +5,22 @@ test('should number', () => {
   expect(result).toBe(5);
 });
 
+test('should 8', async () => {
+  try {
+    await number('0755');
+  } catch (e) {
+    expect(e).toMatch('Аргумент не является числом');
+  }
+});
+
+test('should 16', async () => {
+  try {
+    await number('0xFFFFFFFFFFFFFFFFF');
+  } catch (e) {
+    expect(e).toMatch('Аргумент не является числом');
+  }
+});
+
 test('should error', async () => {
   try {
     await number('test');
